3c67cc70f9aa255644301314f04966300834d895
semr9 <sebastian.moscoso@dynatrace.com>
2025-08-06 21:15:14 -0500

    changes

--- variables/integration15-3-1/variable.js
+++ variables/integration15-3-1/variable.js
@@ -1,8 +1,8 @@
 import patterns from '../patterns.js';
 
 const requiredFieldsIntegration15_3_1 = {
-    "confirmationIds<array1>.value": patterns.alphanumericUppercase,
-    "propertyCode": patterns.alphanumericUppercase
+    "confirmationIds<array1>.value": patterns["alphanumericUppercase"],
+    "propertyCode": patterns["alphanumericUppercase"]
   } 
 
   export default requiredFieldsIntegration15_3_1;
--- variables/integration15-3-2/variable.js
+++ variables/integration15-3-2/variable.js
@@ -1,9 +1,9 @@
 import patterns from '../patterns.js';
 
 const requiredFieldsIntegration15_3_2 = {
-    "confirmationIds<array>.value": patterns.alphanumericUppercase,
-    "propertyCode": patterns.alphanumericUppercase,
-    "guestInformation.altCustId": patterns.alphanumericWithSpecial
+    "confirmationIds<array>.value": patterns["alphanumericUppercase"],
+    "propertyCode": patterns["alphanumericUppercase"],
+    "guestInformation.altCustId": patterns["alphanumericWithSpecial"]
   }
 
   export default requiredFieldsIntegration15_3_2;
